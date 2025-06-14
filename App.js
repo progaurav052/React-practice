824fd86a7bdf455a8d73bd4fb7e6f30f7ee2ffaa
gaurav pai <gaurav511pai@gmail.com>
2025-06-14 13:31:55 +0530

    Created Nested components

--- App.js
+++ App.js
@@ -1,3 +1,17 @@
-const heading = React.createElement("h1", {id:"heading"}, "Hello world with React"); // this "heading is just an javascript object and not h1 element"
+//const heading = React.createElement("h1", { id: "heading" }, "Hello world with React"); // this "heading is just an javascript object and not h1 element"
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading); // converts the heading object into element and renders inside the root
+//root.render(heading); // converts the heading object into element and renders inside the root
+
+// create an nested structure 
+/*
+<div id="top">
+    <div id="parent">
+       <h1></h1>
+    </div>
+</div>
+*/
+
+const jsonobject = React.createElement("div", { id: "top" }, React.createElement("div", { id: "parent" }, React.createElement("h1", { id: "heading" }, "Nested component")));
+root.render(jsonobject);
+
+//there should be only one root which is used to render 
